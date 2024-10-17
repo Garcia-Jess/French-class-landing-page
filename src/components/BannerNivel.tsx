c9132a7673596fa81c949ec5baa59db57e410a95
Jessica Garcia <webdev.jessgarcia@gmail.com>
2024-10-17 18:44:29 -0300

    Added some changed on BannerNivel

--- src/components/BannerNivel.tsx
+++ src/components/BannerNivel.tsx
@@ -3,17 +3,17 @@ import { FaArrowAltCircleRight } from "react-icons/fa";
 import Image from "next/image";
 export default function BannerNivel() {
   return (
-    <div className="bg-blue-200 px-10 lg:px-20 mt-10 pb-20 md:flex">
+    <div className="bg-blue-200 px-10 lg:px-20 mt-10 pb-20 md:flex lg:gap-5">
       <div className="lg:ml-20">
         <div className="text-2xl md:text-4xl lg:text-6xl font-bold text-start mt-10 pt-20 text-gray-800">
           Ainda não sabe seu nível de francês/inglês? Faça o teste agora e
           descubra!
         </div>
-        <div className="flex items-center gap-3 mt-5 text-blue-700 font-bold lg:text-2xl">
-          <a className="" href="">
+        <div className="flex items-center mt-5 text-blue-700 font-bold lg:text-2xl">
+          <a className="flex items-center gap-3" href="">
             Fazer teste
+            <FaArrowAltCircleRight />
           </a>
-          <FaArrowAltCircleRight />
         </div>
       </div>
       <div className="lg:mr-20">
